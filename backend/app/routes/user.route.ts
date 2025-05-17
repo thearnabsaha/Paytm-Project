1313cd2cd4573ffe24d1f5382a6726582f356bdb
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-17 18:55:55 +0530

    fix: add jwtAuth middleware to user and filter routes

--- backend/app/routes/user.route.ts
+++ backend/app/routes/user.route.ts
@@ -7,8 +7,8 @@ const router = Router();
 router.post("/signup", UserSignup);
 router.post("/signin", UserSignin);
 router.get("/user", jwtAuth, userValue);
-router.get("/user/:username", OtherUserValue);
-router.get("/filter", FilterUsers);
+router.get("/user/:username", jwtAuth,OtherUserValue);
+router.get("/filter", jwtAuth,FilterUsers);
 router.post("/send/:username", jwtAuth, sendMoney);
 
 export default router;
