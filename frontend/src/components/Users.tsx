96b8fe654de4b80637ba85fc0b59211172fabee6
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-20 19:44:25 +0530

    feat: add Dialog component and integrate it into Users component for sending money functionality

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -1,6 +1,13 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
-
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 const Users = () => {
     return (
         <div className="flex justify-between pt-5">
@@ -11,7 +18,18 @@ const Users = () => {
                 </Avatar>
                 <h1 className="pl-3 text-xl">User 1</h1>
             </div>
-            <Button>Send Money</Button>
+            <Dialog>
+                <DialogTrigger><Button>Send Money</Button></DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle className="text-2xl">Send Money</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
