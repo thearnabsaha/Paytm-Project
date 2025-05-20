47f910cedc5c44529b0265f4bcba1112e4348075
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-21 01:08:03 +0530

    feat: enhance Send Money dialog with user details and amount input

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -8,6 +8,7 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { Input } from "./ui/input"
 const Users = () => {
     return (
         <div className="flex justify-between pt-5">
@@ -22,11 +23,20 @@ const Users = () => {
                 <DialogTrigger><Button>Send Money</Button></DialogTrigger>
                 <DialogContent>
                     <DialogHeader>
-                        <DialogTitle className="text-2xl">Send Money</DialogTitle>
-                        <DialogDescription>
-                            This action cannot be undone. This will permanently delete your account
-                            and remove your data from our servers.
-                        </DialogDescription>
+                        <DialogTitle className="text-2xl text-center">Send Money</DialogTitle>
+                        <div className="flex items-center">
+                            <Avatar className="size-12">
+                                <AvatarImage src="" />
+                                <AvatarFallback>AS</AvatarFallback>
+                                {/* <AvatarFallback>{firstname[0] || "".toUpperCase()}{lastname[0] || "".toUpperCase()}</AvatarFallback> */}
+                            </Avatar>
+                            <h1 className="text-2xl pl-2">Arnab Saha</h1>
+                        </div>
+                        <p>Amount (in Rs):</p>
+                        <div>
+                            <Input placeholder="Enter Amount" className="h-[40px]"/>
+                            <Button className="w-full mt-3 h-[40px]">Initiate Transfer</Button>
+                        </div>
                     </DialogHeader>
                 </DialogContent>
             </Dialog>
