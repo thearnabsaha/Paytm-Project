216833a2a69baeec9f79c39f07e010848e003fca
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-21 10:04:09 +0530

    feat: add HoverCard component and integrate it into Users component; update package dependencies

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -3,38 +3,61 @@ import { Button } from "./ui/button"
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { Badge } from "@/components/ui/badge"
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
 import { Input } from "./ui/input"
-const Users = () => {
+type UsersProps = {
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+};
+const Users = ({ username, firstname, lastname, email }: UsersProps) => {
+    console.log(username, firstname, lastname, email)
     return (
         <div className="flex justify-between pt-5">
             <div className="flex items-center">
-                <Avatar>
-                    <AvatarImage src="" />
-                    <AvatarFallback>CK</AvatarFallback>
-                </Avatar>
-                <h1 className="pl-3 text-xl">User 1</h1>
+                <HoverCard>
+                    <HoverCardTrigger className=" cursor-pointer">
+                        <Avatar>
+                            <AvatarImage src="" />
+                            <AvatarFallback className=" uppercase">{firstname[0] || "".toUpperCase()}{lastname[0] || "".toUpperCase()}</AvatarFallback>
+                        </Avatar>
+                    </HoverCardTrigger>
+                    <HoverCardContent>
+                        <p>@{username}</p>
+                        <p>{email}</p>
+                    </HoverCardContent>
+                </HoverCard>
+
+                <h1 className="pl-3 text-xl capitalize">{firstname} {lastname}</h1>
             </div>
             <Dialog>
                 <DialogTrigger><Button>Send Money</Button></DialogTrigger>
                 <DialogContent>
                     <DialogHeader>
                         <DialogTitle className="text-2xl text-center">Send Money</DialogTitle>
-                        <div className="flex items-center">
+                        <div className="flex items-center py-3">
                             <Avatar className="size-12">
                                 <AvatarImage src="" />
-                                <AvatarFallback>AS</AvatarFallback>
-                                {/* <AvatarFallback>{firstname[0] || "".toUpperCase()}{lastname[0] || "".toUpperCase()}</AvatarFallback> */}
+                                <AvatarFallback>{firstname[0] || "".toUpperCase()}{lastname[0] || "".toUpperCase()}</AvatarFallback>
                             </Avatar>
-                            <h1 className="text-2xl pl-2">Arnab Saha</h1>
+                            <div>
+                            <h1 className="text-2xl pl-2 capitalize">{firstname} {lastname}</h1>
+                            <span>@{username}</span>
+                            </div>
                         </div>
                         <p>Amount (in Rs):</p>
                         <div>
-                            <Input placeholder="Enter Amount" className="h-[40px]"/>
+                            <Input placeholder="Enter Amount" className="h-[40px]" />
                             <Button className="w-full mt-3 h-[40px]">Initiate Transfer</Button>
                         </div>
                     </DialogHeader>
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,33 +1,40 @@
 import { Input } from "@/components/ui/input"
 import Users from "@/components/Users"
 import { userAtom } from "@/UserAtom"
-// import axios from "axios"
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import { useEffect, useState } from "react"
+// import { useNavigate } from "react-router-dom"
 import { useRecoilState } from "recoil"
-// const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL
 
 const token=localStorage.getItem("token")
 const Dashboard = () => {
   // const navigate=useNavigate()
-  const [data, setData] = useRecoilState(userAtom);
+  const [data, _setData] = useRecoilState(userAtom);
+  const [filteredData, setfilteredData] = useState([{username:"",lastname:"",firstname:"",email:""}])
+  const [inputValue, setinputValue] = useState("")
   useEffect(() => {
-    if(!token){
-      // navigate("/signup")
-    }
+
+    axios.get(`${API_URL}/filter?name=${inputValue}`, { headers: { token: token } })
+    .then((e)=>{
+      setfilteredData(e.data.users)
+    })
+    .catch((e)=>console.log(e))
     
-  }, [])
-  console.log(data)
+  }, [inputValue])
   return (
     <div className="px-10">
       <h1 className="font-bold text-2xl pt-5">Your Balence : {data.balance}</h1>
       <h1 className="font-bold text-xl pt-5 pb-3">Search Users</h1>
-      <Input placeholder="Search for Users here"/>
-      <Users/>
-      <Users/>
-      <Users/>
-      <Users/>
-      <Users/>
+      <Input placeholder="Search for Users here" value={inputValue} onChange={(e)=>{setinputValue(e.target.value)}}/>
+      {
+        filteredData.map((e)=>{
+          return(
+            <Users firstname={e.firstname} lastname={e.lastname} username={e.username} email={e.email}/>
+          )
+        })
+      }
+
     </div>
   )
 }
