b30ae8e793c056fdfcc916ad66c8cdaaaaa10f03
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-23 14:13:27 +0530

    refactor: enhance Profile component layout and data binding

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -11,46 +11,42 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Profile = () => {
-    const [data, setData] = useRecoilState(userAtom);
-
+    const [data, _setData] = useRecoilState(userAtom);
     return (
         <div>
-            {/* <h1>{data.username}</h1>
-        <h1>{data.firstname}</h1>
-        <h1>{data.lastname}</h1>
-        <h1>{data.email}</h1>
-        <h1>{data.balance}</h1> */}
 
             <div className="flex px-10 justify-between py-5">
-                <Card className="w-[40vw]">
+                <Card className="w-[40vw] h-[30vh]">
                     <CardHeader>
                         <CardTitle className="flex flex-col items-center">
                             <Avatar className="size-15">
                                 <AvatarImage src="" />
-                                <AvatarFallback className="text-3xl font-light">AS</AvatarFallback>
+                                <AvatarFallback className="text-3xl font-light uppercase">{data.firstname[0]}{data.lastname[0]}</AvatarFallback>
                             </Avatar>
-                            <h1 className="text-3xl pt-2">Arnab Saha</h1>
+                            <h1 className="text-3xl pt-2">{data.firstname} {data.lastname}</h1>
                         </CardTitle>
                         <CardDescription className="text-center">
-                            <p>@thearnabsaha</p>
+                            <p>@{data.username}</p>
                         </CardDescription>
                     </CardHeader>
                     <CardContent>
-                            <p className="font-bold">Email : <span className="font-light">thearnabsaha201@gmail.com</span></p>
-                            <p className="font-bold">Balance : <span className="font-light">6000</span></p>
+                            <p className="font-bold">Email : <span className="font-light">{data.email}</span></p>
+                            <p className="font-bold">Balance : <span className="font-light">{data.balance} </span></p>
                     </CardContent>
                 </Card>
-                <Card className="w-[50vw]">
-                    <CardHeader>
-                        <CardTitle>Transaction</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
+                <Card className="w-[50vw] h-[80vh]">
+                    <CardHeader className="text-center">
+                        <CardTitle className="text-2xl">Transaction</CardTitle>
+                        <CardDescription>Here you will see all Your Transactions</CardDescription>
                     </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
+                    <CardContent className=" overflow-auto">
+                        <p className="border h-30 m-2 text-center pt-2 rounded-md">Card Content</p>
+                        <p className="border h-30 m-2 text-center pt-2 rounded-md">Card Content</p>
+                        <p className="border h-30 m-2 text-center pt-2 rounded-md">Card Content</p>
+                        <p className="border h-30 m-2 text-center pt-2 rounded-md">Card Content</p>
+                        <p className="border h-30 m-2 text-center pt-2 rounded-md">Card Content</p>
+                        <p className="border h-30 m-2 text-center pt-2 rounded-md">Card Content</p>
                     </CardContent>
-                    <CardFooter>
-                        <p>Card Footer</p>
-                    </CardFooter>
                 </Card>
             </div>
         </div>
