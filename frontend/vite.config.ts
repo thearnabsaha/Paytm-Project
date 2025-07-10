39c41bebdf1fa3c1e70a4bd9b148b735127bc726
thearnabsaha <thearnabsaha201@gmail.com>
2025-07-10 22:03:43 +0530

    chore: add .dockerignore and .gitignore files for backend and frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,15 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    host: true,
+    port: 5173
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['payments.grevelops.co'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
